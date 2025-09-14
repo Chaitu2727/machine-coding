565b7d74ac0a0dfa3c8a9d5ee0d724bac1f8ed27
Chaitanya <vchari999@gmail.com>
2025-09-14 10:58:51 +0530

    Tic Tac Toe game with custom size of the board

---
+++ 14-Tic Tac Toe/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TicToe from "./components/TicToe";
+
+function App() {
+  return (
+    <div>
+      <TicToe size={3} />
+    </div>
+  );
+}
+
+export default App;
---
+++ 14-Tic Tac Toe/src/components/TicToe.jsx
@@ -0,0 +1,106 @@
+import { useEffect, useState } from "react";
+
+const generateResultPattern = (size) => {
+  let result = [];
+  for (let i = 0; i < size; i++) {
+    let horizontal = [];
+    let vertical = [];
+    for (let j = 0; j < size; j++) {
+      horizontal.push(size * i + j);
+      vertical.push(size * j + i);
+    }
+    result.push(horizontal, vertical);
+  }
+
+  let dig1 = [];
+  let dig2 = [];
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+      if (i == j) {
+        dig1.push(size * i + j);
+      }
+      if (i == size - j - 1) {
+        dig2.push(size * i + j);
+      }
+    }
+  }
+  result.push(dig1, dig2);
+
+  return result;
+};
+
+function TicToe({ size = 3 }) {
+  const array = Array(size * size).fill(null);
+  const [winner, setWinner] = useState("");
+  const [buttonArray, setButtonArray] = useState(array);
+  const [xturn, setXturn] = useState(true);
+  const [playerInputs, setPlayerInputs] = useState({
+    X: [],
+    O: [],
+  });
+  const [reultPattern, setReultPattern] = useState([]);
+
+  useEffect(() => {
+    setReultPattern(generateResultPattern(size));
+  }, [size]);
+
+  const winnerCalculations = (player, arr) => {
+    reultPattern.forEach((element) => {
+      const winarr = arr.sort().filter((e) => element.includes(e));
+
+      if (winarr.sort().toString() === element.sort().toString())
+        setWinner(player);
+    });
+  };
+
+  const handleButtonClick = (indexV) => {
+    const updatedButtonArray = buttonArray.map((value, index) => {
+      if (index === indexV) {
+        value = xturn ? "X" : "O";
+        setPlayerInputs((prev) => {
+          const arr = [...prev[value], indexV];
+          winnerCalculations(value, arr);
+          return {
+            ...prev,
+            [value]: arr,
+          };
+        });
+
+        setXturn((prev) => !prev);
+      }
+      return value;
+    });
+    setButtonArray(updatedButtonArray);
+  };
+
+  const handleReset = () => {
+    setButtonArray(array);
+    setXturn(true);
+    setWinner("");
+    setPlayerInputs({ ...playerInputs, X: [], O: [] });
+  };
+
+  return (
+    <div className="app" style={{ "--gridsize": size }}>
+      <h1>Tic Tac Toe</h1>
+      {winner ? <p>{winner} is the Winner</p> : <p>{xturn ? "X" : "O"} Turn</p>}
+      <button onClick={handleReset}>Reset</button>
+      <div className="tic-tac-body">
+        {buttonArray.map((value, index) => {
+          return (
+            <button
+              className="grid-btn"
+              disabled={winner || value}
+              onClick={() => handleButtonClick(index)}
+              key={index}
+            >
+              {value}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default TicToe;
